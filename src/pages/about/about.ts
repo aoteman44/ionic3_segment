1f6e205fe6f6a4432aef35090cb5b362297c236c
aoteman44 <wo8899174@hotmail.com>
2018-05-20 16:46:55 +0800

    2333

---
+++ src/pages/about/about.ts
@@ -0,0 +1,47 @@
+import { Component } from '@angular/core';
+import { NavController } from 'ionic-angular';
+import * as $ from 'jquery';
+
+@Component({
+  selector: 'page-about',
+  templateUrl: 'about.html'
+})
+export class AboutPage {
+
+  list: Object
+
+  constructor(public navCtrl: NavController) {
+    this.list =
+      [{
+        "id": "1",
+        "name": "萝卜",
+        "property": {
+          "name": "luobo",
+          "location": "beijing"
+        }
+      }, {
+        "id": "2",
+        "name": "白菜",
+        "property": {
+          "name": "baicai",
+          "location": "nantong"
+        }
+      }, {
+        "id": "3",
+        "name": "黄瓜",
+        "property": {
+          "name": "huanggua",
+          "location": "hangzhou"
+        }
+      }]
+  }
+
+  fuck(id:any){
+    console.log(id)
+  }
+
+  segmentChanged(e) {
+    this.fuck(e.value)
+  }
+
+}
